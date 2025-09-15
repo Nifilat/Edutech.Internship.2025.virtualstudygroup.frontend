788a0354e9a348b4ebda52c224004baf9b59fbca
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-09-15 11:13:38 +0100

    Refactor icon components: Update stroke properties to camelCase format for consistency across Add, Book, Calendar, DashboardSquare, Students, UserGroup, and UserMultiple icons.

--- src/components/icons/Book.jsx
+++ src/components/icons/Book.jsx
@@ -12,23 +12,23 @@ const Book = (props) => (
     <path
       d="M16.1458 7.7915H8.85417M13.0198 11.9582H8.85315"
       stroke="#717171"
-      stroke-width="1.25"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M20.8333 23.4168H6.24996C5.09937 23.4168 4.16663 22.4841 4.16663 21.3335M4.16663 21.3335C4.16663 20.1829 5.09937 19.2502 6.24996 19.2502H20.8333V6.75016C20.8333 4.78598 20.8333 3.80388 20.2231 3.19369C19.6129 2.5835 18.6308 2.5835 16.6666 2.5835H10.4166C7.47034 2.5835 5.99721 2.5835 5.08192 3.49879C4.16663 4.41408 4.16663 5.88721 4.16663 8.8335V21.3335Z"
       stroke="#717171"
-      stroke-width="1.25"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M20.3125 19.25C20.3125 19.25 19.2709 20.0446 19.2709 21.3333C19.2709 22.6221 20.3125 23.4167 20.3125 23.4167"
       stroke="#717171"
-      stroke-width="1.25"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
