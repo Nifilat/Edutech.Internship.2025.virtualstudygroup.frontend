a98520f1e7c0147ce46bc492c307f7204aba0be5
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-09-16 15:35:26 +0100

    Update ChatWindow component: Modify message display styles for improved readability and visual consistency, including adjustments to background colors and text formatting.

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -86,7 +86,7 @@ function ChatWindow({ activeChat, message, setMessage, handleSendMessage }) {
       </div>
 
       {/* Messages Area */}
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+      <div className="flex flex-col gap-16 flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((msg) => (
           <div
             key={msg.id}
@@ -104,11 +104,11 @@ function ChatWindow({ activeChat, message, setMessage, handleSendMessage }) {
               <div
                 className={`px-4 py-2 rounded-lg ${
                   msg.isOwn
-                    ? 'bg-orange-normal text-white rounded-br-none'
-                    : 'bg-gray-100 text-gray-900 rounded-bl-none'
+                    ? 'bg-orange-light text-black-normal rounded-br-none'
+                    : 'bg-orange-light text-black-normal rounded-bl-none'
                 }`}
               >
-                <p className="text-sm">{msg.message}</p>
+                <p className="text-sm font-normal">{msg.message}</p>
               </div>
             </div>
           </div>
