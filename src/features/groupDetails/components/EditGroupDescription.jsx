a97b126894ddddb1cea54fc7a843b7996915695b
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-10-12 10:21:18 +0100

    feat: add EditGroupDescription component for editing group descriptions with dialog interface

---
+++ src/features/groupDetails/components/EditGroupDescription.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { DialogTitle } from "@radix-ui/react-dialog";
+
+const EditGroupDescription = ({
+  isOpen,
+  onClose,
+  initialDescription,
+  onSave,
+}) => {
+  const [description, setDescription] = useState(initialDescription);
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="max-w-96 p-6 rounded-none w-full">
+        <DialogTitle className="text-black text-base font-medium mb-3">
+          Description
+        </DialogTitle>
+        <Textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="mb-4 text-black-normal text-base rounded-[6px] bg-grey-light-active border-transparent transition-all duration-200 focus:bg-white focus:!border-orange-normal-active  focus-visible:!border-orange-normal-activefocus:shadow-none focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+        <div className="flex justify-end space-x-4">
+          <Button
+            variant="outline"
+            onClick={onClose}
+            className="border border-orange-normal text-sm font-medium text-orange-normal rounded-[3px]"
+          >
+            Cancel
+          </Button>
+          <Button
+            className="bg-orange-normal text-white-normal text-sm font-medium rounded-[3px]"
+            onClick={() => onSave(description)}
+          >
+            Save
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default EditGroupDescription;
